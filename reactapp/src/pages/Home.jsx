4032a8ac2c73b87c5e6de7da079b4573da23802f
Arthur Carvalho <contatocarvalhoarthur@gmail.com>
2023-01-05 13:30:16 -0300

    feat: add css on site

--- reactapp/src/pages/Home.jsx
+++ reactapp/src/pages/Home.jsx
@@ -1,7 +1,11 @@
 function Home() {
 
   return (
+   <>
    <h1>Lista de Presença</h1>
+   <input type="text" placeholder="Digite seu nome..."/>
+   <button type="button">Adicionar</button>
+   </>
   )
 }
 
