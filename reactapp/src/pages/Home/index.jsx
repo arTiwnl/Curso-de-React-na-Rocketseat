27439fbc8c328d772b07a2d487a3658983568b3f
Arthur Carvalho <contatocarvalhoarthur@gmail.com>
2023-01-05 13:58:32 -0300

    feat: font and personalization added

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -3,11 +3,11 @@ import './style.css'
 export function Home() {
 
   return (
-   <>
+   <div className='container'>
    <h1>Lista de Presença</h1>
    <input type="text" placeholder="Digite seu nome..."/>
    <button type="button">Adicionar</button>
-   </>
+   </div>
   )
 }
 
