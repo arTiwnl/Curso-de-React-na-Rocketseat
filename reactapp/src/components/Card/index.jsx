359cd056652be34f47fe0e231b78de9701af23bf
Arthur Carvalho <contatocarvalhoarthur@gmail.com>
2023-01-10 13:52:24 -0300

    feat: add student component

---
+++ reactapp/src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export function Card(){
+    return(
+        <div className='card'>
+            <strong>Arthur Carvalho</strong>
+            <small>13:39:24</small>
+        </div>
+    )
+}
--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -1,12 +1,18 @@
 import './style.css'
 
+import { Card } from '../../components/Card'
+
 export function Home() {
 
   return (
-   <div className='container'>
-   <h1>Lista de Presença</h1>
-   <input type="text" placeholder="Digite seu nome..."/>
-   <button type="button">Adicionar</button>
+    <div className='container'>
+    <h1>Lista de Presença</h1>
+    <input type="text" placeholder="Digite seu nome..."/>
+    <button type="button">Adicionar</button>
+
+    <Card />
+    <Card />
+    <Card />
    </div>
   )
 }
